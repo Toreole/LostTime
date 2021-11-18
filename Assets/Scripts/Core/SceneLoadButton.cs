d9abd995319b73d0dda38c0932751c4caa36f660
Toreole <toreole.official@gmail.com>
2021-11-18 22:25:54 +0100

    Basic Scene Loading and Unloading

---
+++ Assets/Scripts/Core/SceneLoadButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace LostTime.Core
+{
+    //Extremely basic implementation of this concept. Expand on it as needed.
+    public class SceneLoadButton : SceneLoadBehaviour, IInteractable
+    {
+        public void Interact(FPSController player)
+        {
+            LoadUnload();
+        }
+    }
+}
---
+++ Assets/Scripts/Core/SceneManagement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LostTime.Core
+{
+    //Add this component to some disconnected object in the main menu.
+    public class SceneManagement : MonoBehaviour
+    {
+        private static SceneManagement instance;
+
+        [SerializeField]
+        private CanvasGroup loadScreen;
+
+        private void Start()
+        {
+            if (instance == null)
+                instance = this;
+            else return;
+        }
+
+        /// <summary>
+        /// Loads a single scene. (Yes, this unloads all currently active scenes)
+        /// Also includes a loading screen.
+        /// </summary>
+        public static void GotoScene(string sceneName)
+        {
+            instance._GotoScene(sceneName);
+        }
+        private void _GotoScene(string sceneName)
+        {
+            StartCoroutine(Load());
+            IEnumerator Load()
+            {
+                for(float t = 0; t < 0.5f; t += Time.deltaTime)
+                {
+                    loadScreen.alpha = t / 0.5f;
+                    yield return null;
+                }
+
+                AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+                operation.allowSceneActivation = false;
+
+                yield return new WaitUntil(() => operation.progress >= 0.9f);
+                operation.allowSceneActivation = true;
+
+                for (float t = 0; t < 0.5f; t += Time.deltaTime)
+                {
+                    loadScreen.alpha = 1.0f - t / 0.5f;
+                    yield return null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Loads a scene in the background. 
+        /// </summary>
+        public static void LoadScene(string sceneName)
+            => SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+        /// <summary>
+        /// Asynchronously unloads the target scene.
+        /// </summary>
+        public static void UnloadScene(string sceneName)
+            => SceneManager.UnloadSceneAsync(sceneName);
+
+    }
+}
