13b9e4fc653cd92e0162c48ec5f1ddebf0ae8727
Toreole <toreole.official@gmail.com>
2021-11-18 17:57:03 +0100

    Simple Implementation for VoiceOvers with Subtitles

---
+++ Assets/Scripts/Audio/VoiceOver.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LostTime.Audio
+{
+    public class VoiceOver : ScriptableObject
+    {
+        [SerializeField]
+        private AudioClip audioClip;
+        [SerializeField]
+        private TranscriptionLine[] transcription;
+
+        public AudioClip AudioClip => audioClip;
+        public TranscriptionLine[] Transcription => transcription;
+
+    }
+
+    [System.Serializable]
+    public class TranscriptionLine
+    {
+        public string line;
+        public float duration;
+    }
+}
